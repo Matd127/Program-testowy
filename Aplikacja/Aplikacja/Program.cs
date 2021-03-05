34d8cdfce69ae1e7aa6a32c14ca8a92c79a1cbf4
Matd127 <mateusz333555@gmail.com>
2021-03-05 21:44:12 +0100

    Program

---
+++ Aplikacja/Aplikacja/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aplikacja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 15;
+        }
+    }
+}
